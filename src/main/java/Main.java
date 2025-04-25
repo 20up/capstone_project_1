775c8f4469a3a9f8c365a479089ad03b274185d1
Panom <panompreston@gmail.com>
2025-04-25 16:23:47 -0700

    fix files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
-package src.main.java;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.Writer;
 import java.util.Scanner;
 
@@ -14,10 +15,11 @@ public class Main {
 
     public static void home_screen() {
         boolean option = true;
+
         while (option) {
             try {
                 System.out.println("""
-                        Please choose a Option.
+                        |Please choose a Option|
                         D) Add Deposit
                         P) Make Payment (Debit)
                         L) Ledger
@@ -26,7 +28,7 @@ public class Main {
                 System.out.print("Enter:");
                 //choose an option and auto go to uppercase
                 String choice = scanner.nextLine().toUpperCase();
-
+                //checks the variable "choice" to see if it matches any of the case values
                 switch(choice){
                     case "D":
                         add_deposit();
@@ -38,14 +40,13 @@ public class Main {
                         ledger();
                         break;
                     case "X":
-                        System.out.println("thanks for coming");
+                        exit();
                         option = false;
                         break;
                     default:
                         System.out.println("!!Input Not Available!!"+"\n");
                         break;
                 }
-
             } catch (Exception e) {
                 scanner.nextLine();
                 System.out.println("!!exception!!");
@@ -55,6 +56,16 @@ public class Main {
     }
 
     public static void add_deposit(){
+        System.out.println("\n");
+        System.out.print("How much are you depositing:");
+        double amount = scanner.nextDouble();
+
+        try {
+            FileWriter deposit_file = new FileWriter("deposit_file");
+            deposit_file.write("");
+        } catch (IOException e) {
+            System.out.println("Exception warning");
+        }
 
     }
 
@@ -66,9 +77,9 @@ public class Main {
 
     }
 
-    public static void  exit(){
-
-    }
+    public static void exit(){
+      System.out.println("Thanks trying the app \uD83D\uDE3C");
+  }
 
 
 
---
+++ src/main/java/Objects.java
@@ -0,0 +1,20 @@
+package src.main.java;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Objects {
+    private LocalDate present;
+
+    LocalTime time;
+
+    String description;
+
+    String vendor;
+
+    double amount;
+
+
+
+
+}
