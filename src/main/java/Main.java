8cf9274cb47fe24ceaf40fd821889ff16d1be1c0
Panom <panompreston@gmail.com>
2025-04-28 09:57:48 -0700

    progress for (makePayment)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,29 @@
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.Writer;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Main {
     static Scanner scanner = new Scanner(System.in);
 
+    static DateTimeFormatter dtf1 = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+    static FileWriter transaction;
+
+    static {
+        try {
+            transaction = new FileWriter("transaction.csv", true);
+        } catch (IOException e) {
+            System.out.println("Exception warning A");
+            ;
+        }
+    }
+
     public static void main(String[] args) {
         home_screen();
-
     }
 
     public static void home_screen() {
@@ -28,14 +42,14 @@ public class Main {
                 //choose an option and auto go to uppercase
                 String choice = scanner.nextLine().toUpperCase();
                 //checks the variable "choice" to see if it matches any of the case values
-                switch(choice){
+                switch (choice) {
                     case "D":
                         add_deposit();
                         System.out.println("Deposit Successful" + "\n");
                         break;
                     case "P":
-                        make_payment();
-
+                        make_payments();
+                        System.out.println("card information saved" + "\n");
                         break;
                     case "L":
                         ledger();
@@ -55,19 +69,21 @@ public class Main {
         }
     }
 
-    public static void add_deposit(){
+    public static void add_deposit() {
         boolean D = true;
-
-        while(D) {
+        while (D) {
             System.out.println("\n");
+            System.out.println("Please enter a description, vendor, and amount for you deposit.");
+            System.out.print("Description:");
+            String ad_description = scanner.nextLine();
+            System.out.print("Vendor:");
+            String ad_vendor = scanner.nextLine();
             System.out.print("Deposit Amount:");
             double amount = scanner.nextDouble();
             scanner.nextLine();
             try {
-                FileWriter deposit_file = new FileWriter("deposit_file", true);
-
-                deposit_file.write("Deposit: " + amount + "\n");
-                deposit_file.close();
+                transaction.write(LocalDate.now() + " | " + LocalTime.now().format(dtf1) + " | " + ad_description + " | " + ad_vendor + " | $" + amount + "\n");
+                transaction.close();
                 D = false;
             } catch (IOException e) {
                 scanner.nextLine();
@@ -76,39 +92,42 @@ public class Main {
         }
     }
 
-    public static void make_payment(){
+    public static void make_payments() {
+        boolean mp = true;
+        //this is a loop for the credit/debit information 
+        while (mp) {
+            try {
+                System.out.println("Please enter debit information to make payment");
+                System.out.print("Debit/Credit #:");
+                String card_number = scanner.nextLine();
+                if(card_number.length() == 16){
+                    System.out.print("expiration date:");
+                    String card_date = scanner.nextLine();//need to make a convert string to date
+                    System.out.print("card cvv:");
+                    int cvv = scanner.nextInt();
+                    scanner.nextLine();
+                }else
+                    System.out.println("not a valid card number");
+
+            } catch (Exception e) {
+                System.out.println("wong");
+            }
+
+            try {
+                transaction.write("");
+            } catch (IOException e) {
+                System.out.println("!!exception!! mp");
+            }
+        }
+    }
+
+    public static void ledger() {
 
     }
 
-    public static void ledger(){
-
+    public static void exit() {
+        System.out.println("Thanks trying the app \uD83D\uDE3C");
     }
-
-    public static void exit(){
-      System.out.println("Thanks trying the app \uD83D\uDE3C");
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
