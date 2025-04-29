d458a5dda3a9b879bc62b86bc793b64230fdfbf2
Panom <panompreston@gmail.com>
2025-04-29 13:05:06 -0700

    report class

--- src/main/java/Format.java
+++ src/main/java/Format.java
@@ -3,6 +3,7 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class Data_for_format {
+    private String home;
     private LocalDate present;
     private LocalTime time;
     private String description;
@@ -11,13 +12,25 @@ public class Data_for_format {
 
     public LocalDate getPresent() {return present;}
 
+    public void setPresent(LocalDate present) {this.present = present;}
+
     public LocalTime getTime() {return time;}
+
+    public void setTime(LocalTime time) {this.time = time;}
 
     public String getDescription() {return description;}
 
+    public void setDescription(String description) {this.description = description;}
+
     public String getVendor() {return vendor;}
+
+    public void setVendor(String vendor) {this.vendor = vendor;}
 
     public double getAmount() {return amount;}
 
+    public void setAmount(double amount) {this.amount = amount;}
 
+    public void descib_add_d(){
+        System.out.println("Add Deposit");
+    }
 }
